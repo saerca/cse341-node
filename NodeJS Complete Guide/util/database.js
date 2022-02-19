9898fa4fac5b5f7a8f60c44ffe31bb10fd866992
saerca <cas16036@byui.edu>
2022-02-19 15:03:28 -0500

    change db credentials

--- NodeJS Complete Guide/util/database.js
+++ NodeJS Complete Guide/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://user1:vdYXXtsrPA4XLAmY@cluster0.z35cf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
